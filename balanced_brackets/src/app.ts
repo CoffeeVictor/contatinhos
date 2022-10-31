32e771e9499d39e786c72e0a6341d7bdb3a573a7
CoffeeVictor <victor.jgfsilva@gmail.com>
2022-10-31 11:01:08 -0300

    Implements working version of task 1

--- balanced_brackets/src/app.ts
+++ balanced_brackets/src/app.ts
@@ -1,12 +1,46 @@
+type OpeningChar = '(' | '[' | '{';
+type ClosingChar = ')' | ']' | '}';
+type BracketMap = {
+	[key in OpeningChar]: ClosingChar;
+}
+
 export function areBracketsBalanced(input: string): boolean {
 
-    const bracketMap = {
-        '(': ')',
-        '[': ']',
-        '{': '}',
-    }
 
-    const charStack = [];
+	const charSet = ['(', '[', '{', '}', ']', ')'];
+	const hasInvalidCharacters = ![...input].every(char => charSet.includes(char));
 
-    return true
+	if(hasInvalidCharacters) {
+		throw new Error('Input string contains invalid character.');
+	}
+
+	const bracketMap: BracketMap = {
+		'(': ')',
+		'[': ']',
+		'{': '}',
+	}
+
+	const charStack: string[] = [];
+
+	for (const char of input) {
+		const isOpeningBracket = Object.keys(bracketMap).includes(char);
+
+		if (isOpeningBracket) {
+			charStack.push(char);
+		} else {
+			const latestBracket = charStack.pop() as OpeningChar;
+
+			if (!latestBracket) return false;
+
+			const bracketsMatch = bracketMap[latestBracket] === char;
+
+			if(!bracketsMatch) return false;
+		}
+	}
+
+	const unmatchedBrackets = charStack.length !== 0;
+
+	if(unmatchedBrackets) return false;
+
+	return true;
 }
